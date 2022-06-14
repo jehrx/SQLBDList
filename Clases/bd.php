8f83688a491bfbcead6f01b3f89a9ca039fc9cca
jehrx <juanenriquehuertasruiz@gmail.com>
2022-06-14 09:38:03 +0200

    Principio de la clase db

---
+++ Clases/bd.php
@@ -0,0 +1,14 @@
+<?php
+    namespace ITEC\PRESENCIAL\DAW\BDLIST;
+
+    class bd {
+        private string $host = "localhost";
+        private string $user = "root";
+        private string $pass = "1234";
+        private string $db = "bdlist";
+        private string $port = "3306";
+        private string $code = "utf8";
+        private string $dsn = "";
+    }
+
+?>
